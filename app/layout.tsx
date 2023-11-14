87bb07ee606c9b644042e0813a2761a70e80bd6a
Yux <lin.yihang@outlook.de>
2023-11-14 14:43:23 +0100

    chat

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import ToasterContext from "@/app/context/ToasterContext";
+import AuthContext from "@/app/context/AuthContext";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Yakuzan Chat',
+  description: 'This is a real time messenger from yakuza',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body
+          className={inter.className}>
+         <AuthContext>
+              <ToasterContext/>
+              {children}
+         </AuthContext>
+      </body>
+    </html>
+  )
+}
---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+
+import EmptyState from "@/app/components/EmptyState";
+
+const People = () => {
+    return(
+        <div className="hidden lg:block lg:pl-80 h-full border border-amber-400">
+            <EmptyState />
+        </div>
+    );
+}
+
+export default People;
