ae70c32b57ef9502b5dde328ec9d30ea10123329
Yux <lin.yihang@outlook.de>
2023-11-16 13:34:01 +0100

    as
    s

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -18,30 +18,29 @@ const Avatar: React.FC<AvatarProps> = ({
             w-9
             md:h-11
             md:w-11
-            "
-          >
+          ">
               <Image
                   fill
                   src={user?.image || '/images/placeholder.jpg'}
                   alt="Avatar"
               />
           </div>
-          <span
-              className="
-                absolute
-                block
-                rounded-full
-                bg-green-500
-                ring-2
-                ring-white
-                top-0
-                right-0
-                h-2
-                w-2
-                md:h-3
-                md:w-3
-              "
-          />
+              <span
+                  className="
+                    absolute
+                    block
+                    rounded-full
+                    bg-green-500
+                    ring-2
+                    ring-white
+                    top-0
+                    right-0
+                    h-2
+                    w-2
+                    md:h-3
+                    md:w-3
+                  "
+              />
       </div>
   )
 }
