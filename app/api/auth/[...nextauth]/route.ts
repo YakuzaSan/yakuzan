248a04309d1625fe401ef9212f392b63f27197c2
Yux <lin.yihang@outlook.de>
2023-11-16 14:24:20 +0100

    ada
    da

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -55,10 +55,7 @@ export const authOptions: AuthOptions = {
         })
     ],
     debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
+  
 }
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
